e701b0bd0da352354e9c5110d5ec0c6a9bcf3f05
Unknown <blendaf@kth.se>
2018-02-13 14:50:11 +0100

    Task 2 update

--- src/HTTPEcho.java
+++ src/HTTPEcho.java
@@ -1,9 +1,59 @@
 import java.net.*;
 import java.io.*;
 
+
 public class HTTPEcho {
     public static void main( String[] args) {
-        // Your code here
+
+        ServerSocket serverSocket = new ServerSocket(0);
+
+        BufferedReader inFromServer = new BufferedReader(new InputStreamReader(serverSocket.getInputStream()));
+
+        BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+
+
+        while(true){
+
+        }
+
+
+
+
+
+
+
+
+
+
+        String clientSentence;
+        String capitalizedSentence;
+
+
+
+        try
+        {
+            ServerSocket server = new ServerSocket(0);
+        } catch (IOException e)
+        {
+            // handle exception
+        }
+
+        ServerSocket welcomeSocket = new ServerSocket(6789);
+
+        while(true) {
+            Socket connectionSocket = welcomeSocket.accept();
+            BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+
+            DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+            clientSentence = inFromClient.readLine();
+            capitalizedSentence = clientSentence.toUpperCase() + '\n';
+            outToClient.writeBytes(capitalizedSentence);
+
+        }
+
+
+
+
     }
 }
 
